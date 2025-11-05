7c52552b5aa30cefaebe07100f7cd8751e28f342
BuburEncer <ibn.ibnuqayyim@gmail.com>
2025-11-05 20:05:46 +0700

    collaboration implemented

--- src/api/users/handler.js
+++ src/api/users/handler.js
@@ -7,6 +7,7 @@ class UsersHandler {
 
     this.postUserHandler = this.postUserHandler.bind(this);
     this.getUserByIdHandler = this.getUserByIdHandler.bind(this);
+    this.getUsersByUsernameHandler = this.getUsersByUsernameHandler.bind(this);
   }
 
   async postUserHandler(request, h) {
@@ -75,6 +76,17 @@ class UsersHandler {
       console.error(error);
       return response;
     }
+  }
+
+  async getUsersByUsernameHandler(request, h) {
+    const { username = "" } = request.query;
+    const users = await this._service.getUsersByUsername(username);
+    return {
+      status: "success",
+      data: {
+        users,
+      },
+    };
   }
 }
 
